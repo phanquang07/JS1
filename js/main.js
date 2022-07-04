3a46295c9834e666252913a60e6dddfeac5c7b66
Phan Quang <quangphan0724@gmail.com>
2022-07-05 00:00:34 +0700

    Cau 1, Cau 2

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/**
+ * Câu 1: Tính lương nhân viên
+ *  Sơ đồ 3 khối:
+ * Khối 1: Đầu vào
+ * Khai báo biến
+ * + luongMotNgay: 100.000
+ * + soNgayLam: 28
+ * Khối 2: Xử lý
+ * tongLuong = LuongMotNgay * soNgayLam
+ * Khối 3: Đầu ra
+ * in ra tongLuong
+ * */
+
+var luongMotNgay = 100000
+var soNgayLam = 28
+
+tongLuong = luongMotNgay * soNgayLam
+console.log('Tổng lương: ', tongLuong);
+
+/**
+ * Câu 2: Tính giá trị trung bình
+ * Khối 1: Đầu vào
+ * Khai báo 5 biến gồm 5 số thực
+ * Khối 2: Xử lý
+ * tbCong = (soThuNhat + soThuHai + soThuHai + soThuBa + soThuTu + soThuNam) / 5
+ * Khối 3: Đầu ra
+ * in ra tbCong
+ */
+
+var soThuNhat = 1
+var soThuHai = 2
+var soThuBa = 3
+var soThuTu = 4
+var soThuNam = 5
+
+tbCong = (soThuNhat + soThuNHai + soThuBa + soThuTu + soThuNam) / 5
+
+console.log('Trung bình Cộng: ', tbCong);
+
+
